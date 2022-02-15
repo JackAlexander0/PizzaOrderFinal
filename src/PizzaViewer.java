754a068e5cd34226773c618ba25f4cb57a706f53
Jack Alexander <alexajk@mail.uc.edu>
2022-02-15 17:34:30 -0500

    final

--- src/PizzaViewer.java
+++ src/PizzaViewer.java
@@ -11,6 +11,7 @@ public class PizzaViewer extends JFrame {
     JPanel toppingsPnl;
     JPanel receiptPnl;
     JPanel btnPnl;
+    JPanel northPnl;
 
     JRadioButton thinCrustRB;
     JRadioButton regularCrustRB;
@@ -33,25 +34,38 @@ public class PizzaViewer extends JFrame {
     JButton clearBtn;
 
     int confirm;
+    double totalPrice;
+    double toppingPrice;
+    double taxDB;
+
+    String crustSize;
+    String toppingString;
 
     public PizzaViewer() {
         mainPnl = new JPanel();
-        mainPnl.setLayout(new GridLayout(5,1));
+        mainPnl.setLayout(new GridLayout(3,1));
+
+        northPnl = new JPanel();
+        northPnl.setLayout(new GridLayout(1,3));
 
         createCrustPanel();
-        mainPnl.add(crustPnl);
+        northPnl.add(crustPnl);
 
         createSizePanel();
-        mainPnl.add(sizePnl);
+        northPnl.add(sizePnl);
 
         createToppingsPanel();
-        mainPnl.add(toppingsPnl);
+        northPnl.add(toppingsPnl);
 
-        //createReceiptPanel();
-        //mainPnl.add(receiptPnl);
+        mainPnl.add(northPnl);
+
+        createReceiptPanel();
+        mainPnl.add(receiptPnl);
 
         createButtonPanel();
         mainPnl.add(btnPnl);
+
+
 
         add(mainPnl);
         setSize(600,600);
@@ -87,10 +101,10 @@ public class PizzaViewer extends JFrame {
         sizePnl.setBorder(new TitledBorder(new EtchedBorder(), "Pizza Size"));
 
         pizzaSizeCB = new JComboBox();
-        pizzaSizeCB.addItem("Small");
-        pizzaSizeCB.addItem("Medium");
-        pizzaSizeCB.addItem("Large");
-        pizzaSizeCB.addItem("Super");
+        pizzaSizeCB.addItem("Small $8");
+        pizzaSizeCB.addItem("Medium $12");
+        pizzaSizeCB.addItem("Large $16");
+        pizzaSizeCB.addItem("Super $20");
 
         sizePnl.add(pizzaSizeCB);
 
@@ -99,7 +113,7 @@ public class PizzaViewer extends JFrame {
     private void createToppingsPanel() {
         toppingsPnl = new JPanel();
         toppingsPnl.setLayout(new GridLayout(2,3));
-        toppingsPnl.setBorder(new TitledBorder(new EtchedBorder(),"Pizza Toppings"));
+        toppingsPnl.setBorder(new TitledBorder(new EtchedBorder(),"Pizza Toppings ($1 each)"));
 
         pepperoniCB = new JCheckBox("Pepperoni");
         sausageCB = new JCheckBox("Sausage");
@@ -118,12 +132,17 @@ public class PizzaViewer extends JFrame {
     }
 
     private void createReceiptPanel() {
+        receiptPnl = new JPanel();
+        receiptTA = new JTextArea(20,40);
+        receiptTA.setEditable(false);
+        scroller = new JScrollPane(receiptTA);
+        receiptPnl.add(scroller);
 
     }
 
     private void createButtonPanel() {
         btnPnl = new JPanel();
-        btnPnl.setLayout(new GridLayout(1,3));
+        btnPnl.setLayout(new GridLayout(2,3));
 
         quitBtn = new JButton("Quit");
         quitBtn.addActionListener((ActionEvent ae) -> {
@@ -134,9 +153,111 @@ public class PizzaViewer extends JFrame {
         });
 
         clearBtn = new JButton("Clear");
+        clearBtn.addActionListener((ActionEvent ae) -> {
+            receiptTA.setText("");
+            regularCrustRB.setSelected(true);
+            pizzaSizeCB.setSelectedIndex(0);
+            pepperoniCB.setSelected(false);
+            sausageCB.setSelected(false);
+            olivesCB.setSelected(false);
+            mushroomsCB.setSelected(false);
+            monsterGutsCB.setSelected(false);
+            anchoviesCB.setSelected(false);
+
+        });
 
         orderBtn = new JButton("Order");
+        orderBtn.addActionListener((ActionEvent ae ) -> {
+            receiptTA.append("=========================\n");
+            if (thinCrustRB.isSelected()) {
+                crustSize = "Thin Crust ";
+            }
+            else if (regularCrustRB.isSelected()) {
+                crustSize = "Regular Crust ";
+            }
+            else {
+                crustSize = "Deep-Dish Crust ";
+            }
 
+
+            if (((String) pizzaSizeCB.getSelectedItem()).equals("Small $8")) {
+                totalPrice = totalPrice + 8.00;
+                crustSize += " Small";
+
+            }
+            else if (((String) pizzaSizeCB.getSelectedItem()).equals("Medium $12")) {
+                totalPrice = totalPrice + 12.00;
+                crustSize += " Medium";
+
+            }
+            else if (((String) pizzaSizeCB.getSelectedItem()).equals("Large $16")) {
+                totalPrice = totalPrice + 16.00;
+                crustSize += " Large";
+
+            }
+            else {
+                totalPrice = totalPrice + 20.00;
+                crustSize += " Super";
+
+            }
+            crustSize += "       ";
+            crustSize += totalPrice;
+            receiptTA.append(crustSize + "\n");
+
+            toppingString = "\nToppings:\n";
+
+            if (pepperoniCB.isSelected()) {
+                toppingPrice = toppingPrice + 1.00;
+                toppingString += "Pepperoni\n";
+
+            }
+            if (sausageCB.isSelected()) {
+                toppingPrice = toppingPrice + 1.00;
+                toppingString += "Sausage\n";
+
+            }
+            if (olivesCB.isSelected()) {
+                toppingPrice = toppingPrice + 1.00;
+                toppingString += "Olives\n";
+
+            }
+            if (mushroomsCB.isSelected()) {
+                toppingPrice = toppingPrice + 1.00;
+                toppingString += "Mushrooms\n";
+
+            }
+            if (monsterGutsCB.isSelected()) {
+                toppingPrice = toppingPrice + 1.00;
+                toppingString += "Monster Guts\n";
+
+            }
+            if (anchoviesCB.isSelected()) {
+                toppingPrice = toppingPrice + 1.00;
+                toppingString += "Anchovies";
+
+            }
+
+            totalPrice = totalPrice + toppingPrice;
+
+            toppingString += "                          ";
+            toppingString += toppingPrice;
+            receiptTA.append(toppingString + "\n");
+            receiptTA.append("\n");
+            receiptTA.append("Sub-Total:                         " + (totalPrice) + "\n");
+            taxDB = totalPrice * .07;
+            totalPrice = totalPrice + taxDB;
+            receiptTA.append("Tax:                                    " + taxDB);
+            receiptTA.append("\n=========================\n");
+            receiptTA.append("Total:                                " + totalPrice);
+            receiptTA.append("\n=========================\n");
+            totalPrice = 0;
+            toppingPrice = 0;
+
+        });
+
+        btnPnl.add(new JLabel(""));
+        btnPnl.add(new JLabel(""));
+        btnPnl.add(new JLabel(""));
         btnPnl.add(orderBtn);
         btnPnl.add(clearBtn);
         btnPnl.add(quitBtn);
